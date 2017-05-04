e205e1c430e91ad877ade29adfb2fe52a9d07512
sarleon <sarleon56@hotmail.com>
2017-05-04 15:12:29 +0800

    add alot

--- main.c
+++ main.c
@@ -15,20 +15,36 @@ typedef unsigned long uint_64;  //8字节
 
 
 struct BPB{
-  uint_16  BPB_BytsPerSec;    //每扇区字节数
-  uint_8   BPB_SecPerClus;    //每簇扇区数
-  uint_16  BPB_RsvdSecCnt;    //Boot记录占用的扇区数
-  uint_8   BPB_NumFATs;   //FAT表个数
-  uint_16  BPB_RootEntCnt;    //根目录最大文件数
-  uint_16  BPB_TotSec16;
-  uint_8   BPB_Media;
-  uint_16  BPB_FATSz16;   //FAT扇区数
-  uint_16  BPB_SecPerTrk;
-  uint_16  BPB_NumHeads;
-  uint_32  BPB_HiddSec;
-  uint_32  BPB_TotSec32;  //如果BPB_FATSz16为0，该值为FAT扇区数
-}bpb;
+    char ignore[11];
+    uint_16  bytes_per_sector;    //每扇区字节数
+    uint_8   sector_per_cluster;    //每簇扇区数
+    uint_16  reserved;    //Boot记录占用的扇区数
+    uint_8   fat_numbers;   //FAT表个数
+    uint_16  root_entry_count;    //根目录最大文件数
+    uint_16  toal_sector_count;
+    uint_8   ignore_again;
+    uint_16  sector_number;   //FAT扇区数
+    uint_16  sector_per_track;
+    uint_16  number_of_head;
+    uint_32  ignore_this;
+    uint_32  toal_sector_count_fat32;  //如果BPB_FATSz16为0，该值为FAT扇区数
+    }bpb;
 
+struct root_entry{
+    char filename[8];
+    char extension[3];
+    uint_8 attributes;
+    uint_16 reserved;
+    uint_16 creation_time;
+    uint_16 creation_date;
+    uint_16 last_access_date;
+    uint_16 ignore_in_fat12;
+    uint_16 last_write_date;
+    uint_16 last_write_time;
+    uint_16 first_logic_cluster;
+    uint_32 file_size;
+    
+};
 
 char buf[DISK_SIZE];
 
@@ -37,6 +53,7 @@ void print_path(char* str);
 void read_boot_sector();
 int main(){
 
+    
 
 
 
@@ -48,17 +65,56 @@ int main(){
 
   // main part: open the disk file
 
+  
   FILE* f=fopen("a.img","rb");
-  fgets(buf,DISK_SIZE,f);
+  fread(buf,sizeof(char),DISK_SIZE,f);
 
   // read boot sector
   memcpy(&bpb,&buf,sizeof(bpb));
-  printf("%d",bpb.BPB_FATSz16);
-  // handle user input
+  printf("%d\n",bpb.sector_per_track); 
+  printf("%d\n",bpb.bytes_per_sector); 
+  printf("%d\n",bpb.root_entry_count);
+  
+  
+  
+  // read FAT
+  
+
+  // read root dir
+  int root_dir_head = 19 * 512;//byte
+  int entry_size = 32; //byte
+  int root_dir_size = 14 * 512;
+  int entry_count = root_dir_size/entry_size;
+  int current_entry_head=root_dir_head;
+  char* current_entry_pointer=buf+9728;
+  
+  struct root_entry entry;
+  for(int i=0;i<entry_count ; i++){
+
+
+      if(buf[current_entry_head]==0){
+          current_entry_head+=entry_size;
+          current_entry_pointer+=32;
+          continue;
+      }
+
+      memcpy(&entry ,current_entry_pointer ,entry_size);
+
+
+      printf("%s\n",entry.filename);
+      printf("%s\n",entry.extension);
+      
+      current_entry_head+=entry_size;
+      current_entry_pointer+=(32/4);
+  }
+  
+  // user input
   char input[30];
   while(1){
     scanf("%s",&input);
     print_path(input);
+
+    
   }
 
 
